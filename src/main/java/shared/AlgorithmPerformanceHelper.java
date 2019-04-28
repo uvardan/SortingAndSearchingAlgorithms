dcd2f84f947bfb2a471340619951990e65e1c24a
uvardan <uvardan@utexas.edu>
2019-04-27 22:57:48 -0500

    Performance Analyzer Class Implementation to Compare Benchmark

--- src/main/java/shared/AlgorithmPerformanceHelper.java
+++ src/main/java/shared/AlgorithmPerformanceHelper.java
@@ -1,4 +1,271 @@
 package shared;
 
+import sortingAlgorithms.InsertionSort;
+import sortingAlgorithms.MergeSort;
+import sortingAlgorithms.QuickSort;
+import sortingAlgorithms.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+
 public class AlgorithmPerformanceHelper {
+    public void testInsertionSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed = 0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "InsertionSort_SortedInDescendingOrder"+Integer.toString(size);
+        InsertionSort insert= new InsertionSort();
+        for(int i=0;i<10;i++) {
+            int []array=new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            insert.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("InsertionSort time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+    }
+
+    public void testMergeSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start, finish, timeElapsed, totaltimeElapsed = 0;
+        ArrayList<Long> totalTime = new ArrayList<Long>();
+        String Algorithm = "MergeSort_" + Integer.toString(size);
+        MergeSort merge = new MergeSort();
+        for (int i = 0; i < 10; i++) {
+            int[] array = Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            merge.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("MergeSort time " + i + ": " + timeElapsed);
+            totalTime.add(timeElapsed);
+            if (!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for (int j = 0; j < totalTime.size(); j++) {
+            if (j >= 3) {
+                totaltimeElapsed += totalTime.get(j);
+            }
+        }
+        String time = "time: " + Long.toString(totaltimeElapsed / totalTime.size() - 3);
+        Infolist.put(Algorithm, time);
+    }
+    public void testQuickSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "QuickSort_"+Integer.toString(size);
+        QuickSort quick= new QuickSort();
+        for(int i=0;i<10;i++) {
+            int []array= new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            quick.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("QuickSort time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+    }
+    public void testShellSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "ShellSort_"+Integer.toString(size);
+        ShellSort shell= new ShellSort();
+        for(int i=0;i<10;i++) {
+            int []array= new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            shell.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("ShellSort time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+    }
+    public void  testTimSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "TimSort_SortedInDescendingOrder"+Integer.toString(size);
+        TimSort tim= new TimSort();
+        for(int i=0;i<10;i++) {
+            int []array=new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            tim.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("TimSort time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+    }
+    public void testRadixSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "Radix_"+Integer.toString(size);
+        RadixSort radix= new RadixSort();
+        for(int i=0;i<10;i++) {
+            start = System.currentTimeMillis();
+            radix.sortArray(Arrays.copyOf(arr, size));
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("Radix time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+    }
+    public void testHeapSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "Heap_"+Integer.toString(size);
+        HeapSort heap= new HeapSort();
+        for(int i=0;i<10;i++) {
+            int []array=new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            heap.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("Heap time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+
+    }
+
+    public void TestJavaUtilSort(int arr[],HashMap<String,String> Infolist, int size) {
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "JavaUtil_"+Integer.toString(size);
+
+        for(int i=0;i<10;i++) {
+            start = System.currentTimeMillis();
+            Arrays.sort(Arrays.copyOf(arr, size));
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("Heap time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+
+    }
+    public void testBubbleSort(int arr[],HashMap<String,String> Infolist, int size) {
+
+        long start,finish,timeElapsed, totaltimeElapsed =0;
+        ArrayList<Long> totalTime= new ArrayList<Long>();
+        String Algorithm= "BubbleSort_"+Integer.toString(size);
+        BubbleSort bubble= new BubbleSort();
+        for(int i=0;i<10;i++) {
+            int []array=new int[size];
+            array=Arrays.copyOf(arr, size);
+            start = System.currentTimeMillis();
+            bubble.sortArray(array);
+            finish = System.currentTimeMillis();
+            timeElapsed = finish - start;
+            System.out.println("BubbleSort  time "+i+ ": " +timeElapsed);
+            totalTime.add(timeElapsed);
+            if(!isSorted(array)) {
+                System.out.println("Array unsorted");
+                System.exit(0);
+            }
+        }
+        for(int j=0;j<totalTime.size();j++) {
+            if(j>=3) {
+                totaltimeElapsed+=totalTime.get(j);
+            }
+        }
+        String time = "time: "+Long.toString(totaltimeElapsed/totalTime.size()-3);
+        Infolist.put(Algorithm, time);
+
+    }
+
+    public  boolean isSorted(int[] a)
+    {
+        int size= a.length;
+        int []b= new int [size];
+        b=a;
+        int i;
+        for(i = 0; i < b.length; i++){
+            if (b[i] < b[i+1]) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/shared/RunPerformanceAnalyzer.java
+++ src/main/java/shared/RunPerformanceAnalyzer.java
@@ -1,4 +1,154 @@
 package shared;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class RunPerformanceAnalyzer {
+
+    static HashMap<String,String> InfoList = new HashMap<String,String>();
+
+    public static void main(String[] args) throws FileNotFoundException {
+
+        Scanner scanner = new Scanner(new File("/Users/utkarsh/ALGORITHMS/Project/SortingAndSearchingAlgorithms/testCases/SortedInDescendingOrderCase_10000000.txt"));
+        int [] arr = new int [10000000];
+        int i = 0;
+        while(scanner.hasNextInt())
+        {
+            arr[i++] = scanner.nextInt();
+        }
+        //int size= arr.length;
+        //PerformanceAnalyzer pa = new PerformanceAnalyzer();
+        //pa.testHeapSort(arr, InfoList, size);
+        testThread(arr);
+        for(String key: InfoList.keySet()) {
+            System.out.println(key +" " +InfoList.get(key));
+        }
+    }
+
+    // TODO Auto-generated method stub
+    public static void testThread(int []arr) throws FileNotFoundException
+    {
+        int size= arr.length;
+        AlgorithmPerformanceHelper helper = new AlgorithmPerformanceHelper();
+        //.create a callable for each method
+        Callable<Void> callable1 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                //   pa.testInsertionSort(arr, InfoList, size);
+                return null;
+            }
+        };
+
+        Callable<Void> callable2 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.testMergeSort(arr, InfoList, size);
+                return null;
+            }
+        };
+
+        Callable<Void> callable3 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.TestJavaUtilSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable4 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.testShellSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable5 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.testTimSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable6 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.testRadixSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable7 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+                helper.testHeapSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable8 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+              //   helper.testQuickSort(arr, InfoList, size);
+                return null;
+            }
+        };
+        Callable<Void> callable9 = new Callable<Void>()
+        {
+            @Override
+            public Void call() throws Exception
+            {
+              //  helper.testBubbleSort(arr, InfoList, size);
+                return null;
+            }
+        };
+
+
+        //add to a list
+        List<Callable<Void>> taskList = new ArrayList<Callable<Void>>();
+        taskList.add(callable1);
+        taskList.add(callable2);
+        taskList.add(callable3);
+        taskList.add(callable4);
+        taskList.add(callable5);
+        taskList.add(callable6);
+        taskList.add(callable7);
+        taskList.add(callable8);
+        taskList.add(callable9);
+
+        //create a pool executor with 3 threads
+        ExecutorService executor = Executors.newFixedThreadPool(9);
+
+        try
+        {
+            //start the threads and wait for them to finish
+            executor.invokeAll(taskList);
+        }
+        catch (InterruptedException ie)
+        {
+            //do something if you care about interruption;
+        }
+
+    }
+
+
+
 }
